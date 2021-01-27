ba44b0c9dc6775ac280ac3b3c3de4664cf703a6f
niyiojeyinka <olaniyiojeyinka@gmail.com>
2021-01-27 11:51:23 +0100

    refactor: split navigator routes and redux setup

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './screens/home';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
-
+import AppNavigator from './AppNavigator';
 const store = createStore(allReducers);
-const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
     </Provider>
   );
 };
