671bfd459e73d84d301d8900a98cab36121e4547
niyiojeyinka <olaniyiojeyinka@gmail.com>
2021-01-27 11:53:31 +0100

    test: added first screen test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,55 @@
-/**
- * @format
- */
+import * as React from 'react';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import AppNavigatorWithStore from '../App';
+//use app store
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Test Screen rendered correctly', () => {
+  test('first screen really rendered & contains the header', async () => {
+    const component = <AppNavigatorWithStore />;
+
+    const {findByText} = render(component);
+
+    const header = await findByText('Home');
+
+    expect(header).toBeTruthy();
+  });
+
+  test('filter division is present', async () => {
+    const component = <AppNavigatorWithStore />;
+
+    const {findByText} = render(component);
+
+    const division = await findByText('FILTER');
+    expect(division).toBeTruthy();
+  });
+  test('loading feedback is showing when album is yet to be loaded', async () => {
+    const component = <AppNavigatorWithStore />;
+
+    const {findByText} = render(component);
+
+    const fetchingText = await findByText(/fetching/i);
+    expect(fetchingText).toBeTruthy();
+  });
+
+  test('show albums list when it finished fetching', async () => {
+    const component = <AppNavigatorWithStore />;
+
+    const {getAllByA11yLabel} = render(component);
+    const elements = await waitFor(() => getAllByA11yLabel('each-album'));
+
+    expect(elements.length).toBeGreaterThan(0);
+  });
+  test('pressing on each album take user to album gallery', async () => {
+    const component = <AppNavigatorWithStore />;
+
+    const {getByA11yLabel} = render(component);
+    const element = await waitFor(() => getByA11yLabel('each-album'));
+
+    fireEvent(element, 'press');
+    const newHeader = await findByText('album title');
+    expect(newHeader).toBeTruthy();
+  });
 });
